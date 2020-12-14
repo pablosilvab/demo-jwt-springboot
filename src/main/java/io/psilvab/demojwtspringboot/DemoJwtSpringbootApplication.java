debe41d46c4699032f4e6296db793587b4d72897
Pablo Silva <pablonicolassilvabravo@gmail.com>
2020-12-14 18:38:21 -0300

    first commit

---
+++ src/main/java/io/psilvab/demojwtspringboot/DemoJwtSpringbootApplication.java
@@ -0,0 +1,13 @@
+package io.psilvab.demojwtspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJwtSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJwtSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/psilvab/demojwtspringboot/DemoJwtSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.psilvab.demojwtspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJwtSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
