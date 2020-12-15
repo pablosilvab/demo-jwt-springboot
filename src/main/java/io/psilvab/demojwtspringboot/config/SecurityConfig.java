0db65306566f9ee841a2bc2bbf231e79b748300c
Pablo Silva <pablonicolassilvabravo@gmail.com>
2020-12-15 09:42:22 -0300

    userdetails spring security

--- src/main/java/io/psilvab/demojwtspringboot/config/SecurityConfig.java
+++ src/main/java/io/psilvab/demojwtspringboot/config/SecurityConfig.java
@@ -1,17 +1,22 @@
 package io.psilvab.demojwtspringboot.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import io.psilvab.demojwtspringboot.service.CustomUserDetailsService;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	@Autowired
+	private CustomUserDetailsService userDetailsService;
+	
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(auth);
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {	
+		auth.userDetailsService(userDetailsService);
 	}
 }
--- src/main/java/io/psilvab/demojwtspringboot/repository/UserRepository.java
+++ src/main/java/io/psilvab/demojwtspringboot/repository/UserRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import io.psilvab.demojwtspringboot.entity.User;
 
 public interface UserRepository  extends JpaRepository<User, Integer>{
+
+	User findByUsername(String username);
 	
 	
 }
